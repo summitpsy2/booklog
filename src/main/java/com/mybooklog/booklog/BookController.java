ac5ec127542efb75e224be81958612de3bcca914
summitpsy2 <223067606+summitpsy2@users.noreply.github.com>
2025-09-24 21:05:20 +0900

    4주차책 등록 기능 구현

---
+++ src/main/java/com/mybooklog/booklog/BookController.java
@@ -0,0 +1,38 @@
+package com.mybooklog.booklog;
+
+import com.mybooklog.booklog.domain.Book;
+import com.mybooklog.booklog.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    @GetMapping("/books/add")
+    public String showAddBookForm() {
+        return "add-book-form";
+    }
+
+    @PostMapping("/books/add")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books")
+    public String showBookList(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "book-list";
+    }
+}
