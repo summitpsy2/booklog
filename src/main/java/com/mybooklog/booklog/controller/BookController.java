abdba71da05f49d73da537b331bae51701d48166
summitpsy2 <223067606+summitpsy2@users.noreply.github.com>
2025-10-01 20:37:23 +0900

    5주차 구현

--- src/main/java/com/mybooklog/booklog/controller/BookController.java
+++ src/main/java/com/mybooklog/booklog/controller/BookController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable; // 5주차 추가
+import java.util.Optional; // 5주차 추가
 
 @Controller
 @RequiredArgsConstructor
@@ -34,6 +36,42 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "book-list";
+    }
+
+    @GetMapping("/books/{id}")
+    public String showBookDetail(@PathVariable Long id, Model model) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+
+        if(optionalBook.isPresent()) {
+            model.addAttribute("book", optionalBook.get());
+            return "book-detail";
+        } else {
+            return "redirect:/books";
+        }
+    }
+
+    @GetMapping("/books/{id}/edit")
+    public String showEditBookForm(@PathVariable Long id, Model model) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            model.addAttribute("book", optionalBook.get());
+            return "edit-book-form";
+        } else {
+            return "redirect:/books";
+        }
+    }
+
+    @PostMapping("/books/{id}/edit")
+    public String editBook(@PathVariable Long id, @ModelAttribute Book updatedBook) {
+        updatedBook.setId(id);
+        bookRepository.save(updatedBook);
+        return "redirect:/books/{id}";
+    }
+
+    @PostMapping("/books/{id}/delete")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/books";
     }
 }
 
